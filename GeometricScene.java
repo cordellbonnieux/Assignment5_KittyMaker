663ababc97c4671c191f1fc9c6bbe7d7d2571fc7
Cordell <cordellbonnieux@gmail.com>
2021-10-30 19:56:31 -0700

    Started assignment 5

---
+++ GeometricScene.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class GeometricScene extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Pane root = new Pane();
+		
+		
+		
+	}
+
+}
